e3039beef466dc1a9f7cc5f945ee32400429178a
maisum <smmaisum@gmail.com>
2023-09-06 21:02:07 -0500

    compiled static files

--- heisenberg/heisenberg/settings.py
+++ heisenberg/heisenberg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b0q#e#wl_+!5ol_u9pr$9iba2ay#d&5&zh5aallrh3#6a^98^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heisenberg.onrender.com']
 
 
 # Application definition
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
+STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ heisenberg/staticfiles/elements/elements.js
@@ -0,0 +1,196 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+    const buttons = document.querySelectorAll('.element');
+    let activeButton = null;
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            if (activeButton === this) {
+                // If the same button is clicked again, remove the active state
+                this.classList.remove('active');
+                activeButton = null;
+            } else if (activeButton === null) {
+                // If no button is currently active, activate this one
+                this.classList.add('active');
+                activeButton = this;
+            }
+        });
+    });
+
+
+
+
+
+
+    
+
+
+
+    // Function to update element info
+    function updateElementInfo(symbol, data) {
+        const elementInfo = document.getElementById("element-info");
+        elementInfo.innerHTML = `
+            <div class="stats">
+                <strong>${data.name}</strong><br>
+                Atomic Number: ${data.number}<br>
+                Atomic Mass: ${data.atomic_mass}
+            <div>
+
+            <div class="container">
+                <div class="selected-element">
+                    <span class="selected-number">${data.number}</span><br/>
+                    <br>
+                    <span class="selected-letter">${data.symbol}</span><br/>
+                    <br>
+                    <br>
+                    <span class="selected-name" style="text-align: left;">${data.name}</span>
+                </div>
+                <div class="model">
+                    <model-viewer src="${data.bohr_model_3d}" shadow-intensity="1" camera-controls touch-action="pan-y"></model-viewer>
+                </div>
+            </div>
+
+            <div class="content">
+            
+                <h1>Appearance</h1>
+                ${data.appearance}
+
+                <h1>Uses</h1>
+                ${data.uses}
+
+                <h1>Biological Role</h1>
+                ${data.role}
+                
+                <h1>Natural Abundance</h1>
+                ${data.abundance}
+
+                <h1>History</h1>
+                ${data.history}
+                
+            </div>
+            <br>
+            <br>
+            <p> Data provided by <a href="${data.url}" target="_blank" rel="noopener noreferrer">The Royal Society of Chemistry</a></p>
+        `;
+    }
+
+    // Function to enable hover feature
+    function enableHover() {
+        clickedSymbol = null;
+    }
+
+
+    const elements = document.querySelectorAll(".element");
+    let clickedSymbol = null; // To keep track of the clicked symbol
+
+    // Add a click event listener to each element
+    elements.forEach(function (element) {
+        element.addEventListener("click", function () {
+            // Get the element's symbol (e.g., "H", "He")
+            const symbol = element.querySelector(".letter").textContent;
+
+            // If the same element is clicked again, enable hover and return
+            if (clickedSymbol === symbol) {
+                enableHover();
+                return;
+            }
+
+            if (clickedSymbol === null) {
+                // Fetch data from the JSON file
+                fetch("../static/elements/elements.json")
+                    .then((response) => response.json())
+                    .then((data) => {
+                        // Find the element in the "elements" array by symbol
+                        const elementData = data.elements.find((el) => el.symbol === symbol);
+
+                        if (elementData) {
+                            // Update the element info and store the clicked symbol
+                            updateElementInfo(symbol, elementData);
+                            clickedSymbol = symbol;
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching element data:", error);
+                    });
+            }
+        });
+    });
+
+    // Add a mouseover event listener to each element
+    elements.forEach(function (element) {
+        element.addEventListener("mouseover", function () {
+            // If an element is already clicked, do nothing
+            if (clickedSymbol) {
+                return;
+            }
+
+            // Get the element's symbol (e.g., "H", "He")
+            const symbol = element.querySelector(".letter").textContent;
+
+            // Fetch data from the JSON file
+            fetch("../static/elements/elements.json")
+                .then((response) => response.json())
+                .then((data) => {
+                    // Find the element in the "elements" array by symbol
+                    const elementData = data.elements.find((el) => el.symbol === symbol);
+
+                    if (elementData) {
+                        // Display the element's data in the #element-info div
+                        updateElementInfo(symbol, elementData);
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error fetching element data:", error);
+                });
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+    // Get all elements with the "more" class
+    const moreButtons = document.querySelectorAll(".more");
+
+    // Add a click event listener to each "more" button
+    moreButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+
+
+            // Get all rows with data-hidden="true" within the same table
+            const hiddenRows = document.querySelectorAll("tr[data-hidden='true']");
+
+            // Toggle the display property for each hidden row
+            hiddenRows.forEach(function (row) {
+                if (row.style.display === "none" || row.style.display === "") {
+                    row.style.display = "table-row";
+                } else {
+                    row.style.display = "none";
+                }
+            });
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
